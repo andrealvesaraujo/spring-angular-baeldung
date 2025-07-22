b6fb6ad07719a091eb0e57d9e06388aa1b8ada4f
andrealvesaraujo <andrealvesaraujo.1995@gmail.com>
2025-07-22 15:21:21 -0300

    feat: start back end project

---
+++ src/main/java/com/example/baeldung/spring_angular_baeldung/SpringAngularBaeldungApplication.java
@@ -0,0 +1,13 @@
+package com.example.baeldung.spring_angular_baeldung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularBaeldungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularBaeldungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/baeldung/spring_angular_baeldung/SpringAngularBaeldungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baeldung.spring_angular_baeldung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularBaeldungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
