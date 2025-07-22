7593c065b815fe6972e0d7f77756e0dea7742886
andrealvesaraujo <andrealvesaraujo.1995@gmail.com>
2025-07-22 15:29:36 -0300

    feat: add user repository

---
+++ src/main/java/com/example/baeldung/spring_angular_baeldung/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.baeldung.spring_angular_baeldung.repository;
+
+import com.example.baeldung.spring_angular_baeldung.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {}
+
